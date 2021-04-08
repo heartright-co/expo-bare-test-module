efe151f3a75a4b186bd3b4e1a8f9f642a0f329a6
youngjin <teen14y@gmail.com>
2021-04-08 10:08:28 +0900

    add native module tutorial_2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, NativeModules } from 'react-native';
+
+const { CalendarModule } = NativeModules;
 
 export default function App() {
+  const onPress = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button
+        title="Click to invoke your native module!"
+        color="#841584"
+        onPress={onPress}
+      />
     </View>
   );
 }
--- ios/RCTCalendarModule.h
+++ ios/RCTCalendarModule.h
@@ -8,5 +8,10 @@
 #ifndef RCTCalendarModule_h
 #define RCTCalendarModule_h
 
+#import <React/RCTBridgeModule.h>
+@interface RCTCalendarModule : NSObject <RCTBridgeModule>
+@end
+
+
 
 #endif /* RCTCalendarModule_h */
